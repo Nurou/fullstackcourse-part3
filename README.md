50992e2080dcc1118075be36c657a4f2e485f35f
Joel Hassan <joel.nhn@gmail.com>
2019-07-18 20:40:59 +0300

    ready

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
-# fullstackcourse-part3
-Exercise submissions for part 3
+# fullstackcourse 🔥
+
+<img src="./assets/fslogo.svg" width="250" height="300" align="right"/>
+
+Here are my submissions for Helsinki University's **Full Stack Open** course.
+
+Course Description: *Deep Dive Into Modern Web Development*
+
+Link: https://fullstackopen.com
+
+---
+
+Main technologies covered:
+- React
+- Redux
+- Node.js
+- MongoDB
+- GraphQL
+
+---
+## About Exercise submissions
+
+This repository covers parts 3 of the course. Each exercise has its own README with an image/gif demo the final result.
+
+---
+## Breakdown of Parts & Progress
+### Part 1
+Covered the following topics:
+
+a) Introduction to React
+
+b) Javascript
+
+c) Component state, event handlers
+
+d) A more complex state, debugging React apps
+
+### Exercises
+- [x] courseinfo
+- [x] unicafe
+- [x] anecdotes
+
+---
+### Part 2
+Covered the following topics:
+
+a) Rendering a collection, modules
+
+b) Forms
+
+c) Getting data from server
+
+d) Altering data in server
+
+e) Adding styles to React app
+
+### Exercises
+- [x] Course Content 
+- [x] Data For Countries 
+- [x] Phonebook
--- index.js
+++ index.js
@@ -1,66 +1,62 @@
 const express = require('express');
 // function used to create an express app
 const app = express();
+
+/**
+|--------------------------------------------------
+| Middleware
+|--------------------------------------------------
+*/
+
+// without a body-parser, the body property would be undefined
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
-let notes = [
+// logging request data using morgan
+const morgan = require('morgan');
+
+morgan.token('data', function(req) {
+  return JSON.stringify(req.body);
+});
+
+app.use(
+  morgan(':method :url :status :res[content-length] - :response-time :data')
+);
+
+// cors
+const cors = require('cors');
+
+app.use(cors());
+
+// hard-coded list of phonebook entries
+// would normally exist on server
+let persons = [
   {
-    id: 1,
-    content: 'HTML is easy',
-    date: '2019-05-30T17:30:31.098Z',
-    important: true
+    name: 'Arto Hellas',
+    number: '040-123456',
+    id: 1
   },
   {
-    id: 2,
-    content: 'Browser can execute only Javascript',
-    date: '2019-05-30T18:39:34.091Z',
-    important: false
+    name: 'Ada Lovelace',
+    number: '39-44-5323523',
+    id: 2
   },
   {
-    id: 3,
-    content: 'GET and POST are the most important methods of HTTP protocol',
-    date: '2019-05-30T19:20:14.298Z',
-    important: true
+    name: 'Dan Abramov',
+    number: '12-43-234345',
+    id: 3
+  },
+  {
+    name: 'Mary Poppendieck',
+    number: '39-23-6423122',
+    id: 4
+  },
+  {
+    id: 5,
+    name: 'test',
+    number: '123'
   }
 ];
-
-// defining routes to app
-
-// event handler used to handle
-// HTTP GET reqs made to app's root
-app.get('/', (req, res) => {
-  console.log(req.headers);
-  res.send('<h1>Hello Wold!</h1>');
-});
-
-// handles GET reqs to nodes path
-app.get('/notes', (req, res) => {
-  res.json(notes);
-});
-
-app.get('/notes/:id', (request, response) => {
-  const id = Number(request.params.id);
-  const note = notes.find(note => note.id === id);
-  if (note) {
-    response.json(note);
-  } else {
-    response.status(400).end();
-  }
-});
-
-// delete note
-app.delete('/notes/:id', (request, response) => {
-  const id = Number(request.params.id);
-  notes = notes.filter(note => note.id !== id);
-  response.status(204).end();
-});
-
-const generateID = () => {
-  const maxId = notes.length > 0 ? Math.max(...notes.map(n => n.id)) : 0;
-  // unique note ID
-  return maxId + 1;
-};
 
 // add note
 app.post('/notes', (request, response) => {
@@ -73,6 +69,8 @@ app.post('/notes', (request, response) => {
       error: 'content missing'
     });
   }
+
+  // if body supplied, note crafted from its contents
   const note = {
     // note content - can't be empty
     content: body.content,
@@ -80,13 +78,101 @@ app.post('/notes', (request, response) => {
     date: new Date(),
     id: generateID()
   };
-  // add the new note
+  // add the new note (to db)
   notes = notes.concat(note);
   // return it
   response.json(note);
 });
 
-const PORT = 3001;
+app.get('/', (req, res) => {
+  res.send('<h1>Part3</h1>');
+});
+
+// display all persons
+app.get('/api/persons', (req, res) => {
+  res.send(persons);
+});
+
+// display one person
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find(person => person.id === id);
+  if (person) {
+    // display person entry in JSON
+    res.json(person);
+  } else {
+    res.status(400).end();
+  }
+});
+
+// display info
+app.get('/info', (req, res) => {
+  let message = `Phonebook has info for ${persons.length} people 
+  --
+  ${new Date()}`;
+  res.send(message);
+});
+
+// delete a person
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id);
+  persons = persons.filter(person => person.id !== id);
+  res.status(204).end();
+});
+
+const generateId = () => {
+  const maxId =
+    persons.length > 0 ? Math.max(...persons.map(person => person.id)) : 0;
+  return maxId + 1;
+};
+
+const generateRandomId = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+};
+// add persons
+app.post('/api/persons/', (req, res) => {
+  // person content in request body
+  const body = req.body;
+
+  // is info missing?
+  if (!body.name && body.number) {
+    // return crucial
+    return res.status(400).json({
+      error: 'name or number missing'
+    });
+  }
+
+  // does person with same name already exist?
+  const isAlreadyAdded = persons.find(person => person.name === body.name);
+  if (isAlreadyAdded) {
+    return res.status(400).json({
+      error: 'name must be unique'
+    });
+  }
+
+  // new entry with details from req body
+  const person = {
+    id: generateRandomId(1, 100),
+    name: body.name,
+    number: body.number
+  };
+
+  // add the new entry
+  persons = persons.concat(person);
+  // return added entry
+  res.json(person);
+});
+
+// post-routes middleware
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' });
+};
+
+app.use(unknownEndpoint);
+
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
